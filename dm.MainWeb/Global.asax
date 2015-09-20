be15da96677991477f0f4525cae95fbb2409c34d
Sergey Shubin <serg.shubin@gmail.com>
2015-09-20 10:35:41 +0300

    Первоначальный комит

---
+++ dm.MainWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="dm.MainWeb.MvcApplication" Language="C#" %>
---
+++ dm.MainWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(dm.MainWeb.Startup))]
+namespace dm.MainWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
